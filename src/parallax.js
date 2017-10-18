cd883d2b7f7de611938beac88e464faf5dadc0f4
Leon <leon.strachan@gmail.com>
2017-10-18 03:46:30 +0200

    remove left top abs positioning

--- src/parallax.js
+++ src/parallax.js
@@ -304,8 +304,6 @@ class Parallax {
 
       layer.style.position = index ? 'absolute' : 'relative'
       layer.style.display = 'block'
-      layer.style.left = 0
-      layer.style.top = 0
 
       let depth = helpers.data(layer, 'depth') || 0
       this.depthsX.push(helpers.data(layer, 'depth-x') || depth)
